cea0e835253e3584025c2e31f181ecbed5e8bf41
RyyBotWa <107985796+RyyBotWa@users.noreply.github.com>
2022-06-22 15:46:17 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6285256290078','6285256290078','6285256290078'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6285256290078'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6285256290078' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
@@ -33,11 +33,11 @@ global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+global.youtube = 'https://youtube.com/channel/UCcE6pVvM6O6-7TKjkk3pXtw'
+global.ig = 'https://www.instagram.com/mlnzanz
+global.mygc = 'https://chat.whatsapp.com/JzYWj9hRpFe6KB3ezg69FY'
 global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.email = 'kakunbeli10@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
